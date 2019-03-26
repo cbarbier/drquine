ce87421c8d1ab9902714f629cbb9935f8eec50d7
Cedric BARBIER <cbarbier@e2r5p12.42.fr>
2019-03-26 09:31:08 +0100

    fixed comment diff on colleen.go

--- GO/Colleen.go
+++ GO/Colleen.go
@@ -1,10 +1,13 @@
 package main
 import "fmt"
-func print(str string){fmt.Printf(str, 10, 34, str);}
-func main(){
-        c :="package main%[1]cimport %[2]cfmt%[2]c%[1]c//%[1]c//  comment 1%[1]c//%[1]cfunc print(str string){fmt.Printf(str, 10, 34, str);}%[1]cfunc main(){%[1]c//%[1]c//  comment 2%[1]c//%[1]c        c :=%[2]c%[3]s%[2]c;%[1]c        print(c);%[1]c}";
-        print(c);
-}
 //
 //  comment 1
 //
+func print(str string){fmt.Printf(str, 10, 34, str);}
+func main(){
+//
+//  comment 2
+//
+        c :="package main%[1]cimport %[2]cfmt%[2]c%[1]c//%[1]c//  comment 1%[1]c//%[1]cfunc print(str string){fmt.Printf(str, 10, 34, str);}%[1]cfunc main(){%[1]c//%[1]c//  comment 2%[1]c//%[1]c        c :=%[2]c%[3]s%[2]c;%[1]c        print(c);%[1]c}";
+        print(c);
+}
