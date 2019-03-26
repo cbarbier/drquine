3c3caba2099b58637ea8ac1fe00debdf4d5d141b
cbarbier <cbarbier@student.42.fr>
2019-03-26 08:14:56 +0100

    grace.go working

--- GO/Colleen.go
+++ GO/Colleen.go
@@ -1,13 +1,10 @@
 package main
 import "fmt"
+func print(str string){fmt.Printf(str, 10, 34, str);}
+func main(){
+        c :="package main%[1]cimport %[2]cfmt%[2]c%[1]c//%[1]c//  comment 1%[1]c//%[1]cfunc print(str string){fmt.Printf(str, 10, 34, str);}%[1]cfunc main(){%[1]c//%[1]c//  comment 2%[1]c//%[1]c        c :=%[2]c%[3]s%[2]c;%[1]c        print(c);%[1]c}";
+        print(c);
+}
 //
 //  comment 1
 //
-func print(str string){fmt.Printf(str, 10, 34, str);}
-func main(){
-//
-//  comment 2
-//
-        c :="package main%[0]c import %1$cfmt%2$c//%1$c//  comment 1%1$c//%1$cvoid print(char *str){printf(str, 10, 34, str);}%1$cint main(){%1$c//%1$c//  comment 2%1$c//%1$c        char *c=%2$c%3$s%2$c;%1$c        print(c);%1$c}";
-        print(c);
-}
--- GO/Grace.go
+++ GO/Grace.go
@@ -1,14 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define A 34
-#define B 10
-#define FT() int main(){\
-    char *c="#include <stdio.h>%1$c#include <stdlib.h>%1$c#define A 34%1$c#define B 10%1$c#define FT() int main(){%4$c%1$c    char *c=%2$c%3$s%2$c;%4$c%1$c    FILE *ptr;%4$c%1$c    if (!(ptr = fopen(%2$cGrace_kid.c%2$c,%2$cw+%2$c)))exit(0);%4$c%1$c    fprintf(ptr, c, B, A, c, 92);%4$c%1$c    fclose(ptr);}%1$cFT()%1$c/*%1$c    blabla%1$c*/";\
-    FILE *ptr;\
-    if (!(ptr = fopen("Grace_kid.c","w+")))exit(0);\
-    fprintf(ptr, c, B, A, c, 92);\
-    fclose(ptr);}
-FT()
-/*
-    blabla
-*/
+package main
+import (
+        "fmt"
+        "os"
+        )
+const A = 10
+const B = 34
+func main(){
+        c :="package main%[1]cimport (%[1]c        %[2]cfmt%[2]c%[1]c        %[2]cos%[2]c%[1]c        )%[1]cconst A = 10%[1]cconst B = 34%[1]cfunc main(){%[1]c        c :=%[2]c%[3]s%[2]c%[1]c        f, err := os.Create(%[2]cGrace_kid.go%[2]c)%[1]c        if err != nil {return}%[1]c        fmt.Fprintf(f, c, A, B, c)%[1]c        f.Close()}%[1]c//%[1]c//  comment 1%[1]c//"
+        f, err := os.Create("Grace_kid.go")
+        if err != nil {return}
+        fmt.Fprintf(f, c, A, B, c)
+        f.Close()}
+//
+//  comment 1
+//
